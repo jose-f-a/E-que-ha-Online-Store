013102e8e1d94b2624fb7001503299d7758c9656
pedro <pedro97vieira@gmail.com>
2021-01-02 16:35:49 +0000

    app bar

--- loja_front_side/lojaonline/src/components/AppBar.vue
+++ loja_front_side/lojaonline/src/components/AppBar.vue
@@ -1,11 +1,11 @@
-<template>
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-img
-        lazy-src="https://picsum.photos/id/11/10/6"
-        max-height="57"
-        max-width="134"
-        src="../assets/logo.png"
+ <template>
+  <v-app-bar app>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-img
+      lazy-src="https://picsum.photos/id/11/10/6"
+      max-height="57"
+      max-width="134"
+      src="../assets/logo.png"
     ></v-img>
     <v-autocomplete
       v-model="select"
@@ -20,15 +20,32 @@
       label="Digite o nome de um artigo"
       solo-inverted
     ></v-autocomplete>
+    <div v-if="login">
+      <v-btn depressed>
+        <v-icon> mdi-account </v-icon>
+      </v-btn>
+      <v-btn depressed>
+        <v-icon> mdi-heart-outline </v-icon>
+      </v-btn>
+    </div>
+    <div v-else>
+      <v-btn depressed>
+        <v-icon> mdi-login </v-icon>
+      </v-btn>
+      <v-btn depressed> Registar </v-btn>
+    </div>
     <v-btn depressed>
-        <v-icon>
-            mdi-account
-        </v-icon>
+    <v-icon> mdi-cart-outline </v-icon>
     </v-btn>
-    <v-btn depressed>
-        <v-icon>
-            mdi-cart-outline
-        </v-icon>
-    </v-btn>
-    </v-app-bar>
+  </v-app-bar>
 </template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      login: false,
+    };
+  },
+};
+</script>
