0d30ce9713c281317313bee851f1fa78e1b07f23
José Ferreira <jose-f-a@users.noreply.github.com>
2021-01-02 20:09:39 +0000

    popup adicionado, formulario feito

--- loja_front_side/lojaonline/src/views/Home.vue
+++ loja_front_side/lojaonline/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app id="inspire">
+<<<<<<< HEAD
     <app-bar :isLogin="this.login" :onClickDrawer="this.onClickDrawer" :onClickLoginButton="this.onClickLogin">
     </app-bar>
      <v-navigation-drawer
@@ -12,21 +13,21 @@
       <v-container>
         <login :showDialog="this.loginDialog">
         </login>
+=======
+    <app-bar> </app-bar>
+    <signup> </signup>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+    </v-navigation-drawer>
+    <v-main class="grey lighten-2">
+      <v-container>
+        <signup> </signup>
+>>>>>>> 0def4c7 (popup adicionado, formulario feito)
         <v-row>
           <template v-for="n in 4">
-            <v-col
-              :key="n"
-              class="mt-2"
-              cols="12"
-            >
+            <v-col :key="n" class="mt-2" cols="12">
               <strong>Category {{ n }}</strong>
             </v-col>
-            <v-col
-              v-for="j in 6"
-              :key="`${n}${j}`"
-              cols="6"
-              md="2"
-            >
+            <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
               <v-sheet height="150"></v-sheet>
             </v-col>
           </template>
@@ -36,6 +37,7 @@
   </v-app>
 </template>
 <script>
+<<<<<<< HEAD
   import AppBar from '@/components/AppBar.vue'
   import Login from '@/components/Login.vue'
   export default {
@@ -57,3 +59,15 @@
     }
   }
 </script>
+=======
+import AppBar from "@/components/AppBar.vue";
+import Signup from "../components/Signup.vue";
+export default {
+  data: () => ({ drawer: null }),
+  components: {
+    "app-bar": AppBar,
+    "signup": Signup,
+  },
+};
+</script>
+>>>>>>> 0def4c7 (popup adicionado, formulario feito)
