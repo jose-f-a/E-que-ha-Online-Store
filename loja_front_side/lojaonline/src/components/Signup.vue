7d3ebc31ad3c887ebea35d6f62a5e35179d6b43e
José Ferreira <jose-f-a@users.noreply.github.com>
2021-01-02 21:47:57 +0000

    componente atualizado

--- loja_front_side/lojaonline/src/components/Signup.vue
+++ loja_front_side/lojaonline/src/components/Signup.vue
@@ -1,59 +1,53 @@
 <template>
-  <v-row justify="center">
-    <v-dialog v-model="dialog" persistent max-width="450px">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn color="primary" v-bind="attrs" v-on="on">
-          Open Dialog
-        </v-btn>
-      </template>
+  <v-dialog v-model="showSignupDialog" temporary max-width="600px">
+    <v-card>
+      <v-card-title>
+        <span class="headline">Registar</span>
+      </v-card-title>
 
-      <v-card>
-        <v-card-title>
-          <span class="headline">Registar</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field label="Nome*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Contacto*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Email*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field
-                  :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-                  :rules="[rules.required, rules.min]"
-                  :type="show2 ? 'text' : 'password'"
-                  name="pass"
-                  label="Password"
-                  class="input-group--focused"
-                  @click:append="show2 = !show2"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="blue darken" text @click="dialog = false">
-            Fechar
-          </v-btn>
-          <v-btn color="blue" tile @click="dialog = false"> Registar </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field label="Nome*" required></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field label="Contacto*" required></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field label="Email*" required></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[rules.required, rules.min]"
+                :type="show ? 'text' : 'password'"
+                name="pass"
+                label="Password"
+                class="input-group--focused"
+                @click:append="show = !show"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="blue darken" text @click="dialog = false"> Fechar </v-btn>
+        <v-btn color="blue" tile @click="dialog = false"> Registar </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
+  props: {
+    showSignupDialog:{type: Boolean},
+  },
+
   data() {
     return {
-      dialog: false,
-      show2: false,
+      show: false,
       password: "Password",
       rules: {
         required: (value) => !!value || "Required.",
@@ -64,5 +58,3 @@ export default {
   },
 };
 </script>
-
-<style scoped></style>
