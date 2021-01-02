0def4c76f7f3d3992b5255f3d50b5050d2ee4cce
José Ferreira <jose-f-a@users.noreply.github.com>
2021-01-02 20:09:39 +0000

    popup adicionado, formulario feito

---
+++ loja_front_side/lojaonline/src/components/Signup.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="450px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" v-bind="attrs" v-on="on">
+          Open Dialog
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title>
+          <span class="headline">Registar</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Nome*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Contacto*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Email*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[rules.required, rules.min]"
+                  :type="show2 ? 'text' : 'password'"
+                  name="pass"
+                  label="Password"
+                  class="input-group--focused"
+                  @click:append="show2 = !show2"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="blue darken" text @click="dialog = false">
+            Fechar
+          </v-btn>
+          <v-btn color="blue" tile @click="dialog = false"> Registar </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      show2: false,
+      password: "Password",
+      rules: {
+        required: (value) => !!value || "Required.",
+        min: (v) => v.length >= 8 || "Min 8 characters",
+        emailMatch: () => `The email and password you entered don't match`,
+      },
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- loja_front_side/lojaonline/src/views/Home.vue
+++ loja_front_side/lojaonline/src/views/Home.vue
@@ -1,30 +1,18 @@
 <template>
   <v-app id="inspire">
-    <app-bar>
-    </app-bar>
-     <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      temporary
-    >
-    </v-navigation-drawer> 
+    <app-bar> </app-bar>
+    <signup> </signup>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+    </v-navigation-drawer>
     <v-main class="grey lighten-2">
       <v-container>
+        <signup> </signup>
         <v-row>
           <template v-for="n in 4">
-            <v-col
-              :key="n"
-              class="mt-2"
-              cols="12"
-            >
+            <v-col :key="n" class="mt-2" cols="12">
               <strong>Category {{ n }}</strong>
             </v-col>
-            <v-col
-              v-for="j in 6"
-              :key="`${n}${j}`"
-              cols="6"
-              md="2"
-            >
+            <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
               <v-sheet height="150"></v-sheet>
             </v-col>
           </template>
@@ -35,11 +23,13 @@
 </template>
 
 <script>
-    import AppBar from '@/components/AppBar.vue'
-  export default {
-    data: () => ({ drawer: null }),
-    components:{
-        'app-bar':AppBar
-    }
-  }
+import AppBar from "@/components/AppBar.vue";
+import Signup from "../components/Signup.vue";
+export default {
+  data: () => ({ drawer: null }),
+  components: {
+    "app-bar": AppBar,
+    "signup": Signup,
+  },
+};
 </script>
